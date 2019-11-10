ddefa832e9ff06202da93abbec4534ac402bfd9b
Marco Antonio Cabral Dos Santos Junior <marco.antonio.002@acad.pucrs.br>
2019-11-10 19:05:15 -0300

    Progresso Processador

--- src/main/java/com/matheus_marco/Processador.java
+++ src/main/java/com/matheus_marco/Processador.java
@@ -1,8 +1,15 @@
 package com.matheus_marco;
 
-public class Processador{
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class Processador {
 
     private static Processador processador;
+    private LinkedList<String> conteudoArq = new LinkedList<>();
+    HierarquiaMem hm = HierarquiaMem.getInstance();
 
     private Processador(){}
 
@@ -13,6 +20,39 @@ public class Processador{
         }else{
             return processador;
         }
+    }
+
+    public void leArquivo(){
+        try {
+            //Colocar para abrir pelo arquivo ver com o zanon
+            FileReader arq = new FileReader("enderecos.txt");
+            BufferedReader lerArq = new BufferedReader(arq);
+       
+            String linha = lerArq.readLine();
+            conteudoArq.add(linha); 
+            while (linha != null) {
+              linha = lerArq.readLine();
+              conteudoArq.add(linha);
+            }
+            arq.close();
+          } catch (IOException e) {
+              System.err.printf("Erro na abertura do arquivo: %s.\n",
+                e.getMessage());
+          }
+    }
+
+    public String start(){
+        String result = " ";
+        int demorou = 0;
+        long inicio= System.currentTimeMillis();
+        for(int i=0; i < conteudoArq.size(); i++){
+            //nao sei o que é o enderecoDecimal
+            demorou += hm.getEndereco(conteudoArq.get(i), enderecoDecimal);
+        }
+        long fim= System.currentTimeMillis();
+
+        long time = fim - inicio;
+        
     }
 
     
